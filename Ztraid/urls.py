211a9d41726d9a93af7d51cb7fcc5ada06d7a939
sletten93 <sletten93@live.se>
2019-12-17 17:36:47 +0100

    First commit

---
+++ Ztraid/urls.py
@@ -0,0 +1,15 @@
+from Ztraid.testApp import views
+from rest_framework import routers
+from django.urls import include, path
+from django.contrib import admin
+from django.urls import path
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
