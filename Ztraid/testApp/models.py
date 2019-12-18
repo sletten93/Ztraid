ed8027f3b3e2be25311400dbc1245df8d7d6f7a4
Adam136 <trotskij2@live.se>
2019-12-18 01:24:57 +0100

    Första implementation av första sambandsdiagrammet

--- Ztraid/testApp/models.py
+++ Ztraid/testApp/models.py
@@ -1,19 +1,84 @@
 from django.db import models
 
-class Ztr_User(models.Model):
-    name = models.CharField(max_length=255)
+"""
+
+Django-modeller baserade på det första sambandsdiagrammet.
+
+- Alla primärnycklar tillåts i nuläget skapas av Django och ser ut som nedandför:
+    "id" serial NOT NULL PRIMARY KEY 
+    stycket ovanför är skrivet i PostgreSQL syntax, aktuell syntax beror på vald databastyp i settings.py)
+
+- Kopplingstabeller är implementerade m.h.a models.ManyToManyField
+
+"""
+
+# TODO - max_length bör dubbelkollas
+# TODO - namn-konventioner bör enas om, min IDE har fått diktera tills vidare
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=20)
+    cost = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Devices(models.Model):
+
+    name = models.CharField(max_length=20)
+    OS = models.CharField(max_length=20)
+    CPU = models.CharField(max_length=20)
+    GPU = models.CharField(max_length=20)
+    cam = models.CharField(max_length=20)
+    mic = models.CharField(max_length=20)
+
+    # TODO - de flesta def __str__(self): är placeholder functions, gör dem användbara eller ta bort
+
+    def __str__(self):
+        return self.name
+
+
+class ZtrUser(models.Model):
+
+    name = models.CharField(max_length=20)
     email = models.EmailField(max_length=254)
     role = models.CharField(max_length=1)
     nickname = models.CharField(max_length=20)
     phone = models.CharField(max_length=20)
     salt = models.CharField(max_length=25)
     password = models.CharField(max_length=20)
+    # TODO - önskad implementation av kopplingstabell?
+    user_products = models.ManyToManyField(Product)
+    user_devices = models.ManyToManyField(Devices)
 
     def __str__(self):
         return "{} {}".format(self.name, self.nickname)
 
-class User_Pref(models.Model):
+
+class UserPref(models.Model):
+
+    # TODO on_delete=models.CASCADE <-- passande?
+    user_id = models.OneToOneField(ZtrUser)
     net_speed = models.IntegerField(max_length=10)
-    #TO-DO, fixa en-till-en relation
-    #on_delete=models.CASCADE <-- passande?
-    user_id = models.ForeignKey(Ztr_User)
+
+    def __str__(self):
+        return self.user_id
+
+
+class FAQ(models.Model):
+
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=8000)
+    example = models.CharField(max_length=8000)
+
+    def __str__(self):
+        return self.question
+
+
+class Paragraph(models.Model):
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
