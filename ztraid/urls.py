8eef66637850fa48c0c5d3142af22c3bd269d5d3
Adam136 <trotskij2@live.se>
2019-12-20 21:38:21 +0100

    Lite rengöring, omstrukturering.

--- ztraid/urls.py
+++ ztraid/urls.py
@@ -1,17 +1,16 @@
-from Ztraid.testApp import views
+from base_app import views
 from rest_framework import routers
-from django.urls import include, path
+from django.urls import include
 from django.contrib import admin
 from django.urls import path
 
 router = routers.DefaultRouter()
-router.register(r'users', views.ZtrUserViewSet)
-# router.register(r'groups', views.GroupViewSet)
+router.register(r'Users', views.ZtrUserViewSet)
 router.register(r'FAQ', views.FAQViewSet)
-router.register(r'Paragraph', views.ParagraphViewSet)
-router.register(r'Product', views.ProductViewSet)
+router.register(r'Paragraphs', views.ParagraphViewSet)
+router.register(r'Products', views.ProductViewSet)
 router.register(r'Devices', views.DevicesViewSet)
-router.register(r'UserPref', views.UserPrefViewSet)
+router.register(r'UserPrefs', views.UserPrefViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- ztraid/wsgi.py
+++ ztraid/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Ztraid project.
+WSGI config for ztraid project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ztraid.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ztraid.settings')
 
 application = get_wsgi_application()
