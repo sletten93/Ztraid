074e07230c6c4de2041d84a1dbbd87fd4b4a089e
theAvokado <norstrom94@gmail.com>
2020-01-17 23:05:30 +0100

    Planering osv

---
+++ Frontend/app/src/pages/Ztraider/Ztraider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ztraider = props => {
+    return (
+        <div>
+            <h1>Become a ztraider</h1>
+        </div>
+    );
+}
+
+export default Ztraider;
