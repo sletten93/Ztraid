7f7b763e7d2c301844b3f0780e22942b95f91a9a
Adam <trotskij2@live.se>
2020-01-19 18:36:41 +0100

    FullStackTest1

---
+++ frontend/app/src/pages/About/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const About = props => {
+    return (
+        <div>
+            <h1>About ztraid</h1>
+        </div>
+    );
+}
+export default Home;
---
+++ frontend/app/src/pages/Support/Support.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Support = props => {
+    return (
+        <div>
+            <h1>Remote Support</h1>
+        </div>
+    );
+}
+export default Support;
